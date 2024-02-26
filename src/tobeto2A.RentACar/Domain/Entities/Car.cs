1769aa86ae1d0f411ec8b31a1ba0d506a46da424
aysekardas <aysekardas7@gmail.com>
2024-02-26 23:15:57 +0300

    add entities

---
+++ src/tobeto2A.RentACar/Domain/Entities/Car.cs
@@ -0,0 +1,26 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Car : Entity<Guid>
+{
+    public Car() { }
+   
+    public Guid ColorId { get; set; }
+
+    public Guid ModelId { get; set; }
+
+    public string CarState { get; set; }
+
+    public string Kilometer { get; set; }
+
+    public short ModelYear { get; set; }
+
+    public string Plate { get; set; }
+
+    public Model? Model { get; set; } = null;
+}
---
+++ src/tobeto2A.RentACar/Domain/Entities/Model.cs
@@ -0,0 +1,35 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+
+    public class Model : Entity<Guid>
+    {
+        public Model() { }
+      
+        public short Year { get; set; }
+
+        public decimal DailyPrice { get; set; }
+
+        public Guid BrandId { get; set; }
+
+        public Guid FuelId { get; set; }
+        public Guid TransmissionId { get; set; }
+
+        public Brand? Brand { get; set; } = null; 
+
+        public Fuel? Fuel { get; set; } = null;
+
+        public Transmission? Transmission { get; set; } = null; //one-to-one 
+
+        public ICollection<Car>? Cars { get; set; } = null; //one-to many 
+
+    }
+
+}
