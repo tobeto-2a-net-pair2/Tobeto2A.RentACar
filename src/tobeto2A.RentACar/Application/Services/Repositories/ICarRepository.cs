d30de9fe945393cba8e880dfa0b871f0a3f8b526
aysekardas <aysekardas7@gmail.com>
2024-02-27 22:15:51 +0300

    add entities repositories

---
+++ src/tobeto2A.RentACar/Application/Services/Repositories/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories;
+public interface ICarRepository : IAsyncRepository<Car, Guid>, IRepository<Car, Guid>
+{
+}
